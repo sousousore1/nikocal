23fd0b8ff4a6189d6f3b26504f8c44510c76c25c
齋藤宗市 <s.saito@gloops.com>
2015-12-18 16:00:38 +0900

    add summary

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,5 +12,25 @@ class HomeController < ApplicationController
     @this_monday = @this_day - (@this_day.wday - 1)
     @display_days = DISPLAY_DAYS
     @stamp = current_user.stamps.new
+
+    all_stamps = @users.flat_map do |x|
+      x.stamps
+    end
+
+    todays_stamps = all_stamps.find_all do |x|
+      x.target_date == @this_day
+    end
+
+    @todays_stamps_1 = todays_stamps.find_all do |x|
+      x.status == 1
+    end
+
+    @todays_stamps_2 = todays_stamps.find_all do |x|
+      x.status == 2
+    end
+
+    @todays_stamps_3 = todays_stamps.find_all do |x|
+      x.status == 3
+    end
   end
 end
