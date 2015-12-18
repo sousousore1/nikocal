ec28516030d4dd298e06a80cef7ad245b4274ec7
齋藤宗市 <s.saito@gloops.com>
2015-12-18 10:18:04 +0900

    sort by stamp updated_at desc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:help]
   def index
-    @users = User.all.sort_by do |x| x.stamps.last.updated_at end.reverse
+    @users = User.all.sort_by do |x|
+      if x.stamps.any?
+        x.stamps.last.updated_at
+      else
+        x.created_at
+      end
+    end.reverse
     @this_day = Date.today
     @this_monday = @this_day - (@this_day.wday - 1)
     @display_days = DISPLAY_DAYS
