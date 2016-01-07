ac16066d210ac3a42172ff13a69432667bd7cd53
齋藤宗市 <s.saito@gloops.com>
2016-01-07 22:10:52 +0900

    added todays summary

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -5,24 +5,29 @@ class SummariesController < ApplicationController
     @stamp_of_year = Stamp.all.find_all do |x|
       x.target_date.year == Date.today.year
     end
-    @stamp_of_year_json = convert_to_pie_json @stamp_of_year
+    @stamp_of_year_json = convert_to_pie_json_from @stamp_of_year
 
     @stamp_of_month = Stamp.all.find_all do |x|
       x.target_date.month == Date.today.month
     end
-    @stamp_of_month_json = convert_to_pie_json @stamp_of_month
+    @stamp_of_month_json = convert_to_pie_json_from @stamp_of_month
 
     this_monday = Date.today - (Date.today.wday - 1)
     this_friday = Date.today - (Date.today.wday - 1) + 4
     @stamp_of_week = Stamp.all.find_all do |x|
       this_monday <= x.target_date and x.target_date <= this_friday
     end
-    @stamp_of_week_json = convert_to_pie_json @stamp_of_week
+    @stamp_of_week_json = convert_to_pie_json_from @stamp_of_week
+
+    @stamp_of_today = Stamp.all.find_all do |x|
+      x.target_date == Date.today
+    end
+    @stamp_of_today_json = convert_to_pie_json_from @stamp_of_today
   end
 
   private
 
-  def convert_to_pie_json(stamps)
+  def convert_to_pie_json_from(stamps)
     stamps.group_by do |x|
       x.status_text
     end.map do |x, y|
