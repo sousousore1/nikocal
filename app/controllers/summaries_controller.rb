12cead71422aab090b3dbb5b5c634d36fa0a14a4
齋藤宗市 <s.saito@gloops.com>
2016-01-08 17:20:03 +0900

    refactoring

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -2,6 +2,7 @@ class SummariesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # for year
     @stamp_of_year = Stamp.all.find_all do |x|
       x.target_date.year == Date.today.year
     end
@@ -11,15 +12,18 @@ class SummariesController < ApplicationController
       x.target_date
     end.uniq
 
+    # for month
     @stamp_of_month = Stamp.all.find_all do |x|
       x.target_date.month == Date.today.month
     end
     @stamp_of_month_pie = convert_to_pie_from @stamp_of_month
 
+    # for week
     this_monday = Date.today - (Date.today.wday - 1)
-    this_friday = Date.today - (Date.today.wday - 1) + 4
+    this_friday = this_monday + 4
+    this_week = (this_monday..this_friday)
     @stamp_of_week = Stamp.all.find_all do |x|
-      this_monday <= x.target_date and x.target_date <= this_friday
+      this_week.member? x.target_date
     end
     @stamp_of_week_pie = convert_to_pie_from @stamp_of_week
   end
@@ -27,43 +31,35 @@ class SummariesController < ApplicationController
   private
 
   def convert_to_pie_from(stamps)
-    stamps.group_by do |x|
-      x.status_text
-    end.map do |x, y|
-     [x.to_s, y.count] 
-    end.sort_by do |x, y|
-      x
+    stamps.group_by do |stamp|
+      stamp.status_text
+    end.map do |k, v|
+     [k.to_s, v.count] 
+    end.sort_by do |k, v|
+      k
     end
   end
 
   def convert_to_chart_from(stamps)
-    range = stamps.map do |x|
-      x.target_date
+    target_dates = stamps.map do |stamp|
+      stamp.target_date
     end
-    from = range.min
-    to = range.max
+    target_range = (target_dates.min..target_dates.max)
 
-    stamps1 = (from..to).map do |x|
-      stamps.find_all do |y|
-        y.target_date == x and y.status == 1
+    filter = -> (stamps, status) {
+      target_range.map do |target_date|
+        stamps.find_all do |stamp|
+          stamp.target_date == target_date and
+          stamp.status == status
+        end
+      end.map do |stamps|
+        stamps.count
       end
-    end.map do |x|
-      x.count
-    end
-    stamps2 = (from..to).map do |x|
-      stamps.find_all do |y|
-        y.target_date == x and y.status == 2
-      end
-    end.map do |x|
-      x.count
-    end
-    stamps3 = (from..to).map do |x|
-      stamps.find_all do |y|
-        y.target_date == x and y.status == 3
-      end
-    end.map do |x|
-      x.count
-    end
+    }
+
+    stamps1 = filter.call stamps, 1
+    stamps2 = filter.call stamps, 2
+    stamps3 = filter.call stamps, 3
     [stamps1, stamps2, stamps3]
   end
 end
