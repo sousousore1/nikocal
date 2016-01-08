10783f5ef14778188f7af5637ddf33aed90d03df
齋藤宗市 <s.saito@gloops.com>
2016-01-08 15:59:31 +0900

    added chart

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -5,35 +5,76 @@ class SummariesController < ApplicationController
     @stamp_of_year = Stamp.all.find_all do |x|
       x.target_date.year == Date.today.year
     end
-    @stamp_of_year_json = convert_to_pie_json_from @stamp_of_year
+    @stamp_of_year_pie = convert_to_pie_from @stamp_of_year
+    @stamp_of_year_chart = convert_to_chart_from @stamp_of_year
+    @stamp_of_year_chart_categories = @stamp_of_year.map do |x|
+      x.target_date
+    end.uniq
 
     @stamp_of_month = Stamp.all.find_all do |x|
       x.target_date.month == Date.today.month
     end
-    @stamp_of_month_json = convert_to_pie_json_from @stamp_of_month
+    @stamp_of_month_pie = convert_to_pie_from @stamp_of_month
 
     this_monday = Date.today - (Date.today.wday - 1)
     this_friday = Date.today - (Date.today.wday - 1) + 4
     @stamp_of_week = Stamp.all.find_all do |x|
       this_monday <= x.target_date and x.target_date <= this_friday
     end
-    @stamp_of_week_json = convert_to_pie_json_from @stamp_of_week
-
-    @stamp_of_today = Stamp.all.find_all do |x|
-      x.target_date == Date.today
-    end
-    @stamp_of_today_json = convert_to_pie_json_from @stamp_of_today
+    @stamp_of_week_pie = convert_to_pie_from @stamp_of_week
   end
 
   private
 
-  def convert_to_pie_json_from(stamps)
+  def convert_to_pie_from(stamps)
     stamps.group_by do |x|
       x.status_text
     end.map do |x, y|
      [x.to_s, y.count] 
     end.sort_by do |x, y|
       x
-    end.to_json
+    end
+  end
+
+  def convert_to_chart_from(stamps)
+    range = stamps.map do |x|
+      x.target_date
+    end
+    from = range.min
+    to = range.max
+
+    stamps1 = (from..to).map do |x|
+      stamps.find_all do |y|
+        y.target_date == x and y.status == 1
+      end
+    end.map do |x|
+      x.count
+    end
+    stamps2 = (from..to).map do |x|
+      stamps.find_all do |y|
+        y.target_date == x and y.status == 2
+      end
+    end.map do |x|
+      x.count
+    end
+    stamps3 = (from..to).map do |x|
+      stamps.find_all do |y|
+        y.target_date == x and y.status == 3
+      end
+    end.map do |x|
+      x.count
+    end
+    return [stamps1, stamps2, stamps3]
+
+    stamps.group_by do |x|
+      x.status_text
+    end.map do |k, v|
+      values = v.group_by do |x|
+        x.target_date
+      end.map do |k2, v2|
+        v2.count
+      end
+      [k, values]
+    end
   end
 end
