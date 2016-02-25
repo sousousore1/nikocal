7d8457a017e078ca088cb1d48d68971ea040dc0e
齋藤宗市 <s.saito@gloops.com>
2016-02-25 15:12:30 +0900

    bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @date = params[:month] ? Date.parse(params[:month]) : Date.today
+    @date = params[:month] ? Date.parse(params[:month] + '-1') : Date.today
     @stamps = @user.stamps.find_all do |stamp|
-      stamp.target_date.month == @date.month
+      stamp.target_date.mon == @date.mon
     end
   end
 end
